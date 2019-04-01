cfe0e7b5bff4f499137c8c4b218cb84b1e1bc863
Denis O'Leary <acer@college.oleary.com>
2019-04-01 19:05:00 +0000

    testing & backup

---
+++ install_me_first.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Created By Denis O'Leary
+# Please install first before using the final program!
+# to install please just use ./install_me_first.sh
+
+
+sudo apt-get install python-pip &&
+
