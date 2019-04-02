4092ba280d124b73fa6da31e8e1bf511403edac0
Denis O'Leary <acer@college.oleary.com>
2019-04-02 22:35:51 +0000

    adding a third camera view

--- testing_webcam.py
+++ testing_webcam.py
@@ -1,22 +1,25 @@
 import cv2
 
 
-def show_webcam(mirror=False):
+def show_cam(mirror=False):
     cam = cv2.VideoCapture(1)
     while True:
         ret_val, img = cam.read()
         if mirror:
             img = cv2.flip(img, 1)
             gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            im_floodfill_inv = cv2.bitwise_not(img)
         cv2.imshow('Test Webcam - Colour', img)
         cv2.imshow("Test Webcam - Gray", gray_image)
+        cv2.imshow("test webcam - invert", im_floodfill_inv)
         if cv2.waitKey(1) == ord('q'):
-            break #use the q button to quit the program
+            print("The program is now closed!")
+            break #use the Q button to quit the program
     cv2.destroyAllWindows()
 
 
 def main():
-    show_webcam(mirror=True)
+    show_cam(mirror=True)
 
 
 if __name__ == '__main__':
