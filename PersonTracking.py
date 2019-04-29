bb06cf3439342e0010d2d0156a29e4f1f2dcbfa4
college <acer@college.oleary.com>
2019-04-29 20:34:36 +0000

    Cleaned up code, removed not needed items in code

--- PersonTracking.py
+++ PersonTracking.py
@@ -1,43 +1,40 @@
 # Denis O'Leary / T00191862 / Final year project
 
 import cv2
+import os
 
 
 WEBCAM = 1
 LAP_CAM = 0
 BORDER_WIDTH = 2
 OURSIDE_CAM = "rtsp://cam1.oleary.com/Streaming/Channels/2"
-# outside cam is linked to local hostnames, change to IP or FQDN of camera!
 
 # Importing Haar Cascades into program
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 body_cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 
-cap = cv2.VideoCapture(OURSIDE_CAM)
 
-while True:
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    body = body_cascade.detectMultiScale(gray)
+cap = cv2.VideoCapture(LAP_CAM)
 
-    for (x,y,w,h) in body:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), BORDER_WIDTH)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
+while True: # setting up the system
+    ret, img = cap.read() # pulling feed from webcam
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # converting camera to gray scale
+    body = body_cascade.detectMultiScale(gray, 1.3, 5) # setting the body cascade to read the gray scale image
+
+    for (x,y,w,h) in body: # first checking for body
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), BORDER_WIDTH) # adds a border around body
+        fce_gray = gray[y:y+h, x:x+w] # sets up the
+        fce_color = img[y:y+h, x:x+w]
+
         print("Body Detected on stream")
+        os.system('python SendEmail.py') # sends email about body detection
 
-        faces = face_cascade.detectMultiScale(gray)
+        faces = face_cascade.detectMultiScale(fce_gray)
         for (fx,fy,fw,fh) in faces:
-            cv2.rectangle(img, (fx,fy), (fx+fw, fy+fh), (0,255,0), BORDER_WIDTH)
-            roi_gray = gray[fy:fy+fh, fx:fx+fw]
-            roi_color = img[fy:fy+fh, fx:fx+fw]
-            print("Face detected on stream")
+            cv2.rectangle(fce_color, (fx,fy), (fx+fw, fy+fh), (0,255,0), BORDER_WIDTH)
 
-            #eyes = eye_cascade.detectMultiScale(roi_gray)
-            #for (ex,ey,ew,eh) in eyes:
-               #cv2.rectangle(roi_color, (ex, ey), (ex+ew,ey+eh), (0,0,255), 2)
+            print("Face detected on stream")
 
 
     cv2.imshow('Image rec', img)
